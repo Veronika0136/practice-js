0d9024f5990a0ddb1ee92de3ba6b1c41517b4fbc
Veronika0136 <Artemova331@gmail.com>
2025-01-10 22:24:16 +0200

    Update tasks-module-2-1.js

---
+++ js/tasks-module-2-1.js
@@ -0,0 +1,37 @@
+// Задача-6
+// Напишіть функцію, яка приймає оцінку студента (grade) за шкалою від 0 до 100. Якщо оцінка 90 і більше, поверніть "Відмінно",
+// від 75 до 89 - "Добре", від 50 до 74 - "Задовільно", нижче 50 - "Незадовільно".
+
+// let grade = prompt('Введіть свою оцінку від 1-100');
+
+// function getLevel(grade) {
+//   if (grade >= 90) {
+//     return 'Відмінно';
+//   } else if (grade >= 75 && grade <= 89) {
+//     return 'Добре';
+//   } else if (grade >= 50 && grade <= 74) {
+//     return 'Задовільно';
+//   } else if (grade < 50) {
+//     return 'Незадовільно';
+//   }
+// }
+
+// console.log(getLevel(grade));
+
+//Задача-7
+// Напишіть функцію, яка приймає два параметри: dayOfWeek та isHoliday. Якщо dayOfWeek це "Saturday" або "Sunday" або isHoliday є true,
+// поверніть "Вихідний", в іншому випадку поверніть "Робочий день".
+
+// function isDay(dayOfWeek, isHoliday) {
+//   if (dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday' || dayOfWeek === isHoliday) {
+//     return 'Вихідний';
+//   } else {
+//     return 'Робочий день';
+//   }
+// }
+
+// console.log(isDay('Monday'));
+// console.log(isDay('Thuresday'));
+// console.log(isDay('Saturday'));
+// console.log(isDay('isHoliday'));
+// ???????????????????????????????????????????????????
