02b6cf694a13fd9545f06665dca1fea3376ee96d
Veronika0136 <Artemova331@gmail.com>
2025-01-18 00:33:55 +0200

    Update tasks-module-3-2.js

--- js/tasks-module-3-2.js
+++ js/tasks-module-3-2.js
@@ -73,15 +73,134 @@
 // Напиши функцію яка приймає рядок і повертає цей самий рядок але огорнутий у тег div. Приклад: на вході був рядок "Hello world"
 // а на виході рядок "<div>Hello world<div>"
 
+// function addDiv(str){
+//     return `<div>${str}<div>`;
+// }
+
+// console.log(addDiv("veronika"));
 
 // Напиши функцію, яка приймає рядок і повертає кількість слів у ньому (слова розділені пробілами).
+
+// function calculateWord(str) {
+//   const word = str.split(' ');
+//   return word.length;
+// }
+// console.log(calculateWord('asf asfesdkg sdg df htrhrs'));
+
 // Напиши функцію, яка приймає рядок і повертає його довжину помножену на кількість слів в цьому рядку.
+
+// function calculateWord(str) {
+//   const length = str.length;
+//   console.log(length);
+
+//   const word = str.split(' ');
+//   console.log(word.length);
+
+//   return length * word.length;
+// }
+// console.log(calculateWord('asf asfe sdkg asf'));
+
 // Напиши функцію, яка приймає рядок і повертає кількість голосних літер у ньому (a, e, i, o, u). Використовуйте метод includes та цикл.
+
+// function calculateLetters(str) {
+//   let total = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     if (
+//       str[i].includes('a') ||
+//       str[i].includes('e') ||
+//       str[i].includes('i') ||
+//       str[i].includes('o') ||
+//       str[i].includes('u')
+//     ) {
+//       total += 1;
+//     }
+//   }
+
+//   return total;
+// }
+
+// console.log(calculateLetters('Veronika'));
+
 // Напиши функцію, яка приймає рядок і повертає новий рядок, де слова розташовані у зворотному порядку.
+
+// function changeString(str) {
+//   const arr = str.split(' ');
+
+//   const arr1 = arr.toReversed();
+
+//   const str1 = arr1.join(' ');
+//   return str1;
+// }
+// console.log(changeString('Khrul Veronika Viktirivna'));
+
 // Напиши функцію, яка приймає довільну кількість аргументів і повертає true, якщо всі аргументи є числами, і false, якщо ні.
+
+// function foo() {
+//   for (const element of arguments) {
+//     if (typeof element !== 'number') {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+// console.log(foo(1, 4, 6,  "fsf"));
+
 // Напиши функцію, яка приймає довільну кількість аргументів і повертає їх об'єднаними в один рядок, розділений комами.
+
+// function foo() {
+//   const arr = Array.from(arguments);
+//   return arr.join(',');
+// }
+// console.log(foo('gsd', 'sf', 'gafvfvf', 1));
+
 // Напиши функцію, яка приймає довільну кількість чисел і повертає їхній добуток.
+
+// function foo() {
+//   let res = 1;
+//   for (const element of arguments) {
+//     res *= element;
+//   }
+//   return res;
+// }
+
+// console.log(foo(1, 5, 3, 2));
+
 // Напиши функцію, яка приймає довільну кількість чисел і повертає кількість непарних чисел серед них.
-// Напиши функцію яка приймає довільну кількість рядочків і повертає той рядок що є найдовшим
+
+// function foo() {
+//   let calc = 0;
+//   for (const element of arguments) {
+//     if (element % 2 !== 0) {
+//       calc += 1;
+//     }
+//   }
+//   return calc;
+// }
+
+// console.log(foo(1, 3, 5, 8));
+
+// Напиши функцію яка приймає довільну кількість рядочків і повертає той рядок що є найдовшим.
+
+// function findLongestString() {
+//   let longest = '';
+
+//   for (let i = 0; i < arguments.length; i++) {
+//     if (arguments[i].length > longest.length) {
+//       longest = arguments[i];
+//     }
+//   }
+
+//   return longest;
+// }
+
+// console.log(findLongestString('aaaaa', 'sdad', 'eeeeeee', 'dddddd'));
+
 // Напиши функцію яка приймає довільну кількість рядочків і обєднує їх в один цілий рядок.
-// Напиши функцію яка приймає два числа. Якщо одне з чисел дорівнює нуль то повернути null, в іншому випадку повернути добуток цих двох чисел.
+
+// function foo() {
+//   const arr = Array.from(arguments);
+//   return arr.join('');
+// }
+// console.log(foo('aaa', 'sssss', 'd', 'iii', 'jjjj'));
