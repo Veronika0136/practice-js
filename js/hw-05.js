1394453c6f0e39ac0168f827501c68482e8b6b74
Veronika0136 <Artemova331@gmail.com>
2025-02-03 23:41:41 +0200

    Update hw-05.js

--- js/hw-05.js
+++ js/hw-05.js
@@ -133,7 +133,11 @@
 
 // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
 
-// console.log(
+// const sortByDescendingFriendCount = users => {
+//   return users.toSorted((a, b) => b.friends.length - a.friends.length);
+// };
+
+// console.table(
 //   sortByDescendingFriendCount([
 //     {
 //       name: "Moore Hensley",
@@ -221,44 +225,48 @@
 
 // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
 
+// const getTotalBalanceByGender = (users, gender) => {
+//   return users.filter(user => user.gender === gender).reduce((acc, user) => acc + user.balance, 0);
+// };
+
 // const clients = [
-// 	{
-//     name: "Moore Hensley",
-//     gender: "male",
-//     balance: 2811
+//   {
+//     name: 'Moore Hensley',
+//     gender: 'male',
+//     balance: 2811,
 //   },
 //   {
-//     name: "Sharlene Bush",
-//     gender: "female",
-//     balance: 3821
+//     name: 'Sharlene Bush',
+//     gender: 'female',
+//     balance: 3821,
 //   },
 //   {
-//     name: "Ross Vazquez",
-//     gender: "male",
-//     balance: 3793
+//     name: 'Ross Vazquez',
+//     gender: 'male',
+//     balance: 3793,
 //   },
 //   {
-//     name: "Elma Head",
-//     gender: "female",
-//     balance: 2278
+//     name: 'Elma Head',
+//     gender: 'female',
+//     balance: 2278,
 //   },
 //   {
-//     name: "Carey Barr",
-//     gender: "male",
-//     balance: 3951
+//     name: 'Carey Barr',
+//     gender: 'male',
+//     balance: 3951,
 //   },
 //   {
-//     name: "Blackburn Dotson",
-//     gender: "male",
-//     balance: 1498
+//     name: 'Blackburn Dotson',
+//     gender: 'male',
+//     balance: 1498,
 //   },
 //   {
-//     name: "Sheree Anthony",
-//     gender: "female",
-//     balance: 2764
-//   }
+//     name: 'Sheree Anthony',
+//     gender: 'female',
+//     balance: 2764,
+//   },
 // ];
 
-// console.log(getTotalBalanceByGender(clients, "male")); // 12053
+// console.log(getTotalBalanceByGender(clients, 'male')); // 12053
 
-// console.log(getTotalBalanceByGender(clients, "female")); // 8863
+// console.log(getTotalBalanceByGender(clients, 'female')); // 8863
